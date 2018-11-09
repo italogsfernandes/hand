e5ae271d9ce772beb09d2e76159373ff7146842c
Ítalo Fernandes <italogsfernandes@gmail.com>
2018-11-09 13:02:03 +0100

    hey ho lets go

--- python-real-time-application/app.py
+++ python-real-time-application/app.py
@@ -16,22 +16,19 @@ import sys
 if sys.version_info.major == 3:
     # PyQt5
     from PyQt5.QtWidgets import *
-    from views import base_qt5 as base
+    from views import base_project_main_qt5 as base
     from PyQt5 import QtCore
-    # from PyQt5.QtCore import SIGNAL
-    from views import config_processamento_qt5 as config_window
 elif sys.version_info.major == 2:
     # PyQt4
     from PyQt4.QtGui import *
-    from views import base_qt4 as base
+    from views import base_project_main_qt4 as base
     from PyQt4 import QtCore
     from PyQt4.QtCore import SIGNAL
-    from views import config_processamento_qt4 as config_window
 else:
     print("Versao do python nao suportada")
 # ------------------------------------------------------------------------------
 
-class ContractionDetector(QMainWindow, base.Ui_MainWindow):
+class HandProjectApp(QMainWindow, base.Ui_MainWindow):
     def __init__(self, parent=None):
         super(self.__class__, self).__init__(parent)
         # Configuring UI, seting up chart, etc:
@@ -49,56 +46,28 @@ class ContractionDetector(QMainWindow, base.Ui_MainWindow):
         self.label_replace.setParent(None)
 
         # Setting up inicial conditions:
-        self.cb_emg.toggle() # enable the plot of the emg signal
-        # sets the threshould for contraction detection
-        self.sl_threshould.setValue(0.25)
-        self.sl_threshould_value_changed(10)
+        # None
 
     def setup_signals_connections(self):
-        #self.actionProcessamento.triggered.connect(self.processamento_clicked)
-        self.btn_start.clicked.connect(self.btn_start_clicked)
-        self.btn_calib.clicked.connect(self.btn_calib_clicked)
-        self.sl_threshould.valueChanged.connect(self.sl_threshould_value_changed)
-        self.cb_emg.toggled.connect(lambda x: self.emg_app.plotHandler.emg_bruto.set_visible(x))
-        self.cb_hbt.toggled.connect(lambda x: self.emg_app.plotHandler.hilbert.set_visible(x))
-        self.cb_ret.toggled.connect(lambda x: self.emg_app.plotHandler.hilbert_retificado.set_visible(x))
-        self.cb_env.toggled.connect(lambda x: self.emg_app.plotHandler.envoltoria.set_visible(x))
-        self.cb_lim.toggled.connect(lambda x: self.emg_app.plotHandler.threshold.set_visible(x))
-        self.cb_det.toggled.connect(lambda x: self.emg_app.plotHandler.set_detection_visible(x))
-        #self.connect(setup_form, SIGNAL("proc_changed(QString)"), self.proc_changed)
-
-    def proc_changed(self, new_proc):
-        print(new_proc)
-        self.emg_app.update_proc_type(new_proc)
+        """ Connects the events of objects in the view (buttons, combobox, etc)
+        to respective methods.
+        """
+        self.cb_ch1.toggled.connect(lambda x: self.emg_app.plotHandler.emg_bruto.set_visible(x))
+        self.cb_ch2.toggled.connect(lambda x: self.emg_app.plotHandler.hilbert.set_visible(x))
+        self.cb_ch3.toggled.connect(lambda x: self.emg_app.plotHandler.hilbert_retificado.set_visible(x))
+        self.cb_ch4.toggled.connect(lambda x: self.emg_app.plotHandler.envoltoria.set_visible(x))
 
     def closeEvent(self, q_close_event):
         self.emg_app.stop()
         super(self.__class__, self).closeEvent(q_close_event)
 
-    def processamento_clicked(self):
-        setup_form.show()
-
-    def btn_start_clicked(self):
-        if self.emg_app.started:
-            self.emg_app.stop()
-            self.btn_start.setText('S')
-        else:
-            self.emg_app.start()
-            self.btn_start.setText('P')
-
-    def btn_calib_clicked(self):
-        self.lbl_status.setText(QtCore.QString.fromUtf8("Status: Calibração não implementada."))
-
-    def sl_threshould_value_changed(self, sl_value):
-        self.lbl_threshould.setText("Limiar:\n%.2f" % (sl_value * 1.0 / 100))
-        self.emg_app.plotHandler.set_threshold(sl_value * 1.0 / 100)
-
-app = QApplication(sys.argv)
-setup_form = SetupApp()
-form = ContractionDetector()
-
-
+# ------------------------------------------------------------------------------
 def main():
+    """Main function
+    Instantialize the objects and runs the app.
+    """
+    app = QApplication(sys.argv)
+    form = HandProjectApp()
     form.show()
     app.exec_()
 
